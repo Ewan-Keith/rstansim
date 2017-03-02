ba6a6427bf4280670e670309700c350deb1be253
Ewan Keith <keith.ewan@yahoo.co.uk>
2017-03-02 18:34:41 +0000

    don't run example fixed

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -18,7 +18,7 @@
 #' @return Will either return a fitted stan object or the string "convergence
 #' failed for X attempts" where X is the maxFailure parameter.
 #' @examples
-#' Use \dontrun{
+#' \dontrun{
 #' safeFit(stanModel = "8schools.stan", stanData = schools_dat,
 #' stanIter = 1000, stainChains = 4, maxRhat = 1.05)
 #'}
