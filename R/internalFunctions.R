7c31df3fca08043ee7f703e2c89f4f6b5d9a0bc3
Ewan-Keith <keith.ewan@yahoo.co.uk>
2017-03-02 10:38:52 +0000

    Update internalFunctions.R
    minor build fixes

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -18,7 +18,7 @@
 #' @return Will either return a fitted stan object or the string "convergence
 #' failed for X attempts" where X is the maxFailure parameter.
 #' @examples
-#' safeFit(stanModel = "8schools.stan" stanData = schools_dat,
+#' safeFit(stanModel = "8schools.stan", stanData = schools_dat,
 #' stanIter = 1000, stainChains = 4, maxRhat = 1.05)
 #'
 #' @export
