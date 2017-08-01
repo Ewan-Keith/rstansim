a0424a81d9fd725b607a72796ac3d8512ee1966a
ewan-keith <keith.ewan@yahoo.co.uk>
2017-08-02 00:13:35 +0100

    still developing data simulation. committing just to keep consistecy across machines.

--- R/stansim_simulate.R
+++ R/stansim_simulate.R
@@ -1,15 +1,23 @@
 
-reg_sim <- function(N = 100, run_estimation = 1) {
-  list("N" = N, "x" = rep(0, N), "y" = rep(0, N), "run_estimation" = run_estimation)
+reg_sim <- function(N = 100) {
+  list("N" = N, "x" = rep(0, N), "y" = rep(0, N))
 }
 
-reg_data <- reg_sim(1000, run_estimation = 0)
+reg_data <- reg_sim(1000)
 
 file <- 'tests/testthat/data-raw/simtestreg.stan'
 
 #-----------------------------------------------------------------
 #### stansim_simulate ####
-stansim_simulate <- function(file, save_dir, data = NULL, param_values = NULL, datasets = 1){
+stansim_simulate <-
+  function(file,
+           save_dir,
+           hold_data = NULL,
+           sim_params = "all",
+           param_values = NULL,
+           datasets = 1,
+           sim_drop = TRUE) {
+
 
   #-----------------------------------------------------------------
   #### input checks ####
@@ -36,43 +44,56 @@ stansim_simulate <- function(file, save_dir, data = NULL, param_values = NULL, d
 
   #-----------------------------------------------------------------
   #### run simulations ####
-  simulate_internal(file = file, data = data)
+  simulate_internal(file = file, hold_data = hold_data, sim_params = sim_params,
+                    sim_drop = sim_drop)
 }
 
 
 
-simulate_internal <- function(file, data) {
+simulate_internal <- function(file, hold_data, sim_params, sim_drop) {
   fitted <-
     rstan::stan(
       file = file,
-      data = data,
+      data = hold_data,
       iter = 1,
       chains = 1,
       cores = 1,
       algorithm = "Fixed_param"
     )
 
-  extracted <- extract(fitted)
+  params_and_data <- c(hold_data, rstan::extract(fitted))
 
-  stansim_name_ind <- grepl("^stansim_.+$", names(extracted))
-
-  sim_data <- extracted[stansim_name_ind]
-
-  # remove "stansim_" from name
-  names(sim_data) <-
-    substr(names(sim_data), start = 9, stop = nchar(names(sim_data)))
+  extracted <- params_and_data[sim_params]
 
   # remove unnecesary dimnames attributes
-  remove_attributes <- function(data) {
-    attributes(data)$dimnames <- NULL
-    data
+  remove_attributes <- function(x) {
+    attributes(x)$dimnames <- NULL
+    x
   }
-  lapply(X = sim_data, FUN = remove_attributes)
+  clean_extracted <- lapply(X = extracted, FUN = remove_attributes)
+
+  # if sim_drop then cut "sim_" from all param/data names
+  names(clean_extracted) <-
+    if (sim_drop) {
+      gsub("^sim_", "", names(clean_extracted))
+    } else {
+      names(clean_extracted)
+    }
+
+  clean_extracted
+
 }
 
 
-fit <- stansim_simulate(file = file, data = reg_data, datasets = 2)
+fit <- stansim_simulate(file = file, hold_data = reg_data, datasets = 2, sim_params = c("sim_x", "sim_y", "N"))
 
 extracted <- extract(fit)
 
 plot(extracted$stansim_x, extracted$stansim_y)
+
+
+## refit test
+fit_test <- rstan::stan(file, data = fit)
+
+
+
--- tests/testthat/data-raw/simtestreg.stan
+++ tests/testthat/data-raw/simtestreg.stan
@@ -12,9 +12,9 @@ model {
   y ~ normal(alpha + beta * x, sigma);
 }
 generated quantities {
-  vector[N] stansim_x;
-  vector[N] stansim_y;
+  vector[N] sim_x;
+  vector[N] sim_y;
 
-  for(i in 1:N) stansim_x[i] = normal_rng(0, 1);
-  for(i in 1:N) stansim_y[i] = normal_rng(alpha + stansim_x[i] * beta, 1);
+  for(i in 1:N) sim_x[i] = normal_rng(0, 1);
+  for(i in 1:N) sim_y[i] = normal_rng(alpha + sim_x[i] * beta, 1);
 }
